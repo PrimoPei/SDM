be72f48dd8d5234f06df8489360516d11756eeb2
Radamés Ajna <radamajna@gmail.com>
2022-12-07 23:10:16 +0000

    rooms selector

--- frontend/src/lib/Buttons/RoomsSelector.svelte
+++ frontend/src/lib/Buttons/RoomsSelector.svelte
@@ -34,9 +34,10 @@
 	function changeRoom(room: RoomResponse) {
 		$selectedRoomID = room.room_id;
 		collapsed = true;
-		$page.url.searchParams.set('roomid', room.room_id);
-		window.location.search = $page.url.searchParams.toString()
-		window.parent.postMessage({ queryString: $page.url.searchParams.toString() }, '*');
+		const params = new URLSearchParams(window.location.search);
+		params.set('roomid', room.room_id);
+		window.location.search = params.toString()
+		window.parent.postMessage({ queryString: params.toString() }, '*');
 	}
 </script>
 
