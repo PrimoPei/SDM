9a0a519080f22f06db0853cd2c932051964c17a5
Radamés Ajna <radamajna@gmail.com>
2022-12-07 20:00:18 +0000

    static

---
+++ static/_app/immutable/chunks/0-bf77bb50.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-1df8b34d.js";export{t as component};
--- static/_app/immutable/chunks/1-94ce70ec.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-8ebf176c.js";export{t as component};
