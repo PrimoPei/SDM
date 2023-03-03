70676c198495324b31210cf9ccb7e892e3bf62f8
Radamés Ajna <radamajna@gmail.com>
2023-03-03 04:43:30 +0000

    fix for codespaces

--- frontend/vite.config.dev.ts
+++ frontend/vite.config.dev.ts
@@ -4,10 +4,10 @@ import type { UserConfig } from 'vite';
 const config: UserConfig = {
 	plugins: [sveltekit()],
 	server: {
-		// host: "0.0.0.0",
+		host: "0.0.0.0",
 		proxy: {
 			'/server': {
-				target: 'http://localhost:7860',
+				target: 'http://0.0.0.0:7860',
 				changeOrigin: true,
 				cookieDomainRewrite: 'localhost',
 				rewrite: (path) => path.replace(/^\/server/, '')
